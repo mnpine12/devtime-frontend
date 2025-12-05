83da1b32d4dcf161e07eaef4aaff668347785288
sora.min <sora.min@intelliansys.co.kr>
2025-12-05 15:25:52 +0900

    chore : react 19 보안 이슈 업데이트 및 eslint 와 prettier 환경설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -9,60 +9,67 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores([
-    'node_modules/',
-    'dist/',
-    'build/',
-    'scripts/',
-    'src/routeTree.gen.ts',
-    '*.env',
-    '*.env.local',
-    '*.env.development',
-    '*.env.production',
-    '*.d.ts',
-    'vite.config.ts',
-    'vite.config.js',
-  ]),
-  {
-    name: 'prettier',
-    plugins: {
-      prettier,
-      import: importPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            ['parent', 'sibling', 'index'],
-            'object',
-            'type',
-          ],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
+    globalIgnores([
+        'node_modules/',
+        'dist/',
+        'build/',
+        'scripts/',
+        'src/routeTree.gen.ts',
+        '*.env*',
+        '*.d.ts',
+    ]),
+    {
+        name: 'base-rules',
+        plugins: {
+            import: importPlugin,
         },
-      ],
+        rules: {
+            'arrow-body-style': 'off',
+            'import/prefer-default-export': 'off',
+            'no-shadow': 'off',
+            'camelcase': 'off',
+            'no-nested-ternary': 'off',
+            'class-methods-use-this': 'off',
+            'no-await-in-loop': 'off',
+            'no-unused-vars': 'off',
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'type'],
+                    'newlines-between': 'always',
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+            
+            'react-refresh/only-export-components': 'warn',
+        },
     },
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      prettierConfig,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: {
+            import:importPlugin,
+        },
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            prettierConfig,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            '@typescript-eslint/no-shadow': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn', 
+            '@typescript-eslint/ban-ts-comment': 'warn', 
+            '@typescript-eslint/no-explicit-any': 'warn',
+            
+            'react/react-in-jsx-scope': 'off',
+        }
     },
-  },
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,33 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tsconfigPaths(),
-  ],
+    base: '/',
+    assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.svg', '**/*.png', '**/*.jpg', '**/*.webp'],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@Apis': path.resolve(__dirname, './src/apis'),
+            '@Assets': path.resolve(__dirname, './src/assets'),
+            '@Components': path.resolve(__dirname, './src/components'),
+            '@Features': path.resolve(__dirname, './src/features'),
+            '@Hooks': path.resolve(__dirname, './src/hooks'),
+            '@Pages': path.resolve(__dirname, './src/pages'),
+            '@Routes': path.resolve(__dirname, './src/routes'),
+            '@Stores': path.resolve(__dirname, './src/stores'),
+            '@Types': path.resolve(__dirname, './src/types'),
+            '@Utils': path.resolve(__dirname, './src/utils'),
+        },
+    },
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        react(),
+        tsconfigPaths(),
+    ],
 });
